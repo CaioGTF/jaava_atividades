dc03b1371ef96dc6a7b4fa278e1d8f3c1b530665
Caio Gabriel <105945358+CaioGTF@users.noreply.github.com>
2025-03-20 23:05:45 +0000

    java

--- exemplo_album/Album.java
+++ exemplo_album/Album.java
@@ -1,19 +1,53 @@
 package exemplo_album;
 public class Album {
-    public String name;
-    public String artist;
-    public double price;
-    public int numberOfCopies;
+    private String name;
+    private String artist;
+    private double price;
+    private int numberOfCopies;
     public void sellCopies(){
         if(numberOfCopies > 0){
             numberOfCopies--;
-            System.out.println("Um album foi vendido!");
+            System.out.println("Um álbum foi vendido!");
         }
         else{
-            System.out.println("Não há albuns disponíveis!");
+            System.out.println("Nenhum álbum disponível!");
         }
     }
     public void orderCopies(int num){
         numberOfCopies += num;
     }
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public String getArtist() {
+      return artist;
+   }
+   public void setArtist(String artist) {
+      this.artist = artist;
+   }
+   public double getPrice() {
+      return price;
+   }
+   public void setPrice(double price) {
+      if(price > 0) {
+         this.price = price;          
+      }
+      else {
+         this.price = 0.0;
+      }
+   }
+   public int getNumberOfCopies() {
+      return numberOfCopies;
+   }
+   public void setNumberOfCopies(int numberOfCopies) {
+      if(numberOfCopies > 0) {
+         this.numberOfCopies = numberOfCopies;        
+      }
+      else {
+         this.numberOfCopies = 0;
+      }
+   }
 }
