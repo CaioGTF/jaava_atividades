8249cccc71e33e23cab18ea1e67ee375c02a822d
Caio Gabriel <105945358+CaioGTF@users.noreply.github.com>
2025-03-21 23:02:42 +0000

    java

--- Pessoa/Main.java
+++ Pessoa/Main.java
@@ -1,3 +1,12 @@
 public class Main {
-    
+    public static void main(String[] args) {
+         /*
+       Criando os objetos
+        */
+      // Criando objetos da classe Carro
+      PessoaFisica pessoa1 = new PessoaFisica("123", "10", "Beatriz", "02/02/1999");
+      pessoa1.setNome("Bia");
+      pessoa1.setCPF("128.153.334-71");
+      System.out.println(pessoa1.getData());
+}
 }
